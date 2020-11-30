fa753c697c254bc3a993d1589189a4ea0b888c70
Afonso -aptsharp <aff-on@hotmail.com>
2020-11-30 15:05:31 -0300

    Estrutura simples de PILHA com C#

---
+++ Pilha.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Est_Pilha
+{
+    class Pilha
+    {
+
+        Posicao primeiro;
+
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha()
+        {
+            if(primeiro == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+        
+        class Posicao
+        {
+            public Posicao proximo;
+            public object item;
+            public Posicao(Posicao proximo, object item)
+            {
+                this.proximo = proximo;
+                this.item = item;
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Est_Pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" - Pilha\n");
+
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(1000);
+            s.Empilha(321654987);
+            s.Empilha("asdf");
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+
+
+        }
+    }
+}
